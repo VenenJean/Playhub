71da3cbc2795686522368217fccd847863caca6d
♦ Venen Jean ♦ <102526504+VenenJean@users.noreply.github.com>
2025-11-05 08:58:09 +0100

    Added sql connection point

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database
+{
+    private static $conn = null;
+
+    public static function getConnection()
+    {
+        if (self::$conn === null) {
+            $servername = "(localdb)\\MSSQLLocalDB";
+            $username = "php_user";
+            $password = "StrongPassword123!";
+            $database = "playhub";
+
+            try {
+                self::$conn = new PDO("sqlsrv:server=$servername;Database=$database", $username, $password);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Database connection failed: " . $e->getMessage());
+            }
+        }
+
+        return self::$conn;
+    }
+}
