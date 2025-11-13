6ddfa4f1c0ed27a18246cae2000022049b7795dc
illun0r <anton.kai.programm@gmail.com>
2025-11-13 13:36:47 +0100

    css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PlayHub</title>
     <link rel="icon" href="favicon.svg">
-    
+
     <link rel="stylesheet" href="styles/playhub.css">
 </head>
 
